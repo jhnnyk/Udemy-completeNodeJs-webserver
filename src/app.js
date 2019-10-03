aab337cf11e0cf1ea5b381bc5e293f69e6dddb94
John Knotts <jhnnyk@gmail.com>
2019-10-03 13:02:29 -0600

    the query string

--- src/app.js
+++ src/app.js
@@ -40,9 +40,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    });
+  }
+
   res.send({
     location: 'Evergreen',
-    forecast: '45 degrees and misting'
+    forecast: '45 degrees and misting',
+    address: req.query.address
+  });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
   });
 });
 
